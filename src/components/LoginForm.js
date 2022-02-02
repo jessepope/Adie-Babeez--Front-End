9b91076d28da9a7d4069ceb3631a77fa221c9a52
Elly Wong <elly.yywong@gmail.com>
2022-02-02 12:36:20 -0800

    unified  keys with BE and aligned api calls with BE - SignupForm and LoginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,98 +1,80 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import './LoginForm.css';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./LoginForm.css";
 
-
-function LoginForm(props){
-  const [formField, setFormField] = useState({ userInfo: "", password: "" })
+function LoginForm(props) {
+  const [formField, setFormField] = useState({ email: "", password: "" });
   let navigate = useNavigate();
-  
-  const onUserInfoChange = (e) => {
-    setFormField({
-      ...formField,
-      userInfo: e.target.value
-    });
-  };
 
-  const onPasswordChange = (e) => {
+  const onFieldChange = (e) => {
     setFormField({
       ...formField,
-      password: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
   const onLoginFormSubmit = (e) => {
     e.preventDefault();
-    
+
     /*We are going into the DOM taking specific element which is the GET element by ID we wew taking that element and bringing it into our function as a local varible so we can do things with it.*/
-    const userInfo = document.getElementById('userInfo');
-    const password = document.getElementById('password');
+    const email = document.getElementById("email");
+    const password = document.getElementById("password");
 
-    let validUserInfo = true 
-    let validPassword = true
-    if (formField.userInfo.length ===0 || formField.userInfo.length > 50) {
-      /*ADD CSS FOR THE BORDER TO TURN IT RED MAKE A CSS ID TAG FOR THE BOX*/
-      userInfo.style.borderColor= 'red'; 
-      validUserInfo= false;
-    }; 
-    if (formField.password.length ===0 || formField.password.length > 50) {
-      /*ADD CSS FOR THE BORDER TO TURN IT RED MAKE A CSS ID TAG FOR THE BOX*/
-      password.style.borderColor= 'red';
-      validPassword = false; 
-    };
+    let validEmail = true;
+    let validPassword = true;
+    if (formField.email.length === 0 || formField.email.length > 50) {
+      email.style.borderColor = "red";
+      validEmail = false;
+    }
+    if (formField.password.length === 0 || formField.password.length > 50) {
+      password.style.borderColor = "red";
+      validPassword = false;
+    }
 
-    if (validUserInfo === true && validPassword === true) {axios
-    .get(`${process.env.REACT_APP_BACKEND_URL}/users/login/verify`, formField)
-    .then((response) => {
-      // if login is incorrect then stay on page and give an error message
-      // if login is correct, redirect to homepage
-      if (response.status !== 200) {
-        // give error messgae, stay on login page
-      } else {
-        props.updateCurrUser(response.data)
-        navigate(`/feed`);
-      }
-      setFormField({
-        userInfo: "",
-        password: "",
-      });
-      navigate(`/feed`);
-    })  
-    /*possibly adding logic*/
-    .catch((err) => {
-      console.log(err);
-    });  
-  }};
-
-
-
+    if (validEmail === true && validPassword === true) {
+      console.log(formField);
+      axios
+        .get(`${process.env.REACT_APP_BACKEND_URL}/login`, [formField])
+        .then((response) => {
+          console.log("good");
+          // if login is correct, redirect to homepage
+          props.updateCurrUser(response.data);
+          navigate(`/feed`);
+        })
+        /*possibly adding logic*/
+        .catch(() => {
+          console.log(
+            "Username or email is already used. Please re-enter the form."
+          );
+        });
+    }
+  };
 
   return (
-      <form className="login-form" onSubmit={onLoginFormSubmit}>
-        <input 
-        id="userInfo"
+    <form className="login-form" onSubmit={onLoginFormSubmit}>
+      <input
+        id="email"
         minLength={1}
         maxLength={80}
-        name="userInfo"
-        value= {formField.userInfo}
-        placeholder="email or username" 
-        onChange={onUserInfoChange}
-        ></input>
-        <br></br>
-        <input 
+        name="email"
+        value={formField.email}
+        placeholder="email"
+        onChange={onFieldChange}
+      ></input>
+      <br></br>
+      <input
         id="password"
         minLength={1}
         maxLength={100}
         name="password"
-        value= {formField.password}
+        value={formField.password}
         placeholder="password"
-        onChange={onPasswordChange}
-        ></input>
-        <br></br>
-        <input type="submit" value="Submit" />
-      </form>
-
+        onChange={onFieldChange}
+      ></input>
+      <br></br>
+      <input type="submit" value="Login" />
+    </form>
   );
 }
 export default LoginForm;
--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,80 +1,86 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import './SignUpForm.css';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./SignUpForm.css";
 
-function SignUpForm(props){
-  const [formField, setFormField] = useState({ userName: "", email: "", password: "" , pronouns: "", location: "", className: "", campus: "", bio: "" })
+function SignUpForm(props) {
+  const [formField, setFormField] = useState({
+    username: "",
+    email: "",
+    password: "",
+    pronouns: "",
+    // location: "",
+    class_name: "",
+    campus: "",
+    bio: "",
+  });
   let navigate = useNavigate();
 
   const onFieldChange = (e) => {
     setFormField({
       ...formField,
-      [e.target.name]: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
   const onSignUpFormSubmit = (e) => {
     e.preventDefault();
-    
+
     /*We are going into the DOM taking specific element which is the GET element by ID we wew taking that element and bringing it into our function as a local varible so we can do things with it.*/
-    const userName = document.getElementById('userInfo');
-    const password = document.getElementById('password');
-    const email = document.getElementById('email');
+    const username = document.getElementById("userInfo");
+    const password = document.getElementById("password");
+    const email = document.getElementById("email");
 
-    let validData = true
+    let validData = true;
 
-    if (formField.userName.length ===0 || formField.userName.length > 50) {
+    if (formField.username.length === 0 || formField.username.length > 50) {
       /*ADD CSS FOR THE BORDER TO TURN IT RED MAKE A CSS ID TAG FOR THE BOX*/
-      userName.style.borderColor= 'red'; 
-      validData= false;
-    }; 
-    if (formField.password.length ===0 || formField.password.length > 50) {
+      username.style.borderColor = "red";
+      validData = false;
+    }
+    if (formField.password.length === 0 || formField.password.length > 50) {
       /*ADD CSS FOR THE BORDER TO TURN IT RED MAKE A CSS ID TAG FOR THE BOX*/
-      password.style.borderColor= 'red';
-      validData= false; 
-    };
-    if (formField.email.length ===0 || formField.email.length > 50) {
+      password.style.borderColor = "red";
+      validData = false;
+    }
+    if (formField.email.length === 0 || formField.email.length > 50) {
       /*ADD CSS FOR THE BORDER TO TURN IT RED MAKE A CSS ID TAG FOR THE BOX*/
-      email.style.borderColor= 'red';
-      validData = false; 
-    };
-    if (validData === true) { axios
-    .post(`${process.env.REACT_APP_BACKEND_URL}/signup`, formField)
-    .then(() => {
-      setFormField({
-        userInfo: "",
-        password: "",
-      });
-      navigate(`/feed`);
-    })  
-    /*possibly adding logic*/
-    .catch((err) => {
-      console.log(err);
-    });  
-  }};
-
+      email.style.borderColor = "red";
+      validData = false;
+    }
+    if (validData === true) {
+      console.log(formField);
+      axios
+        .post(`${process.env.REACT_APP_BACKEND_URL}/signup`, [formField])
+        .then(() => {
+          navigate(`/feed`);
+        })
+        /*possibly adding logic*/
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
 
   return (
-  
-      <form className="sign-up-form" onSubmit={onSignUpFormSubmit}>
-
-        <div className="left-half">
-        <h3  className="sign-up-title" >Create a new Adie Babeez account</h3>
-      <p className="sign-up-subt">
-        Please complete the form below. Username, email, and password are
-        required fields.
-      </p>
-      <br></br>
+    <form className="sign-up-form" onSubmit={onSignUpFormSubmit}>
+      <div className="left-half">
+        <h3 className="sign-up-title">Create a new Adie Babeez account</h3>
+        <p className="sign-up-subt">
+          Please complete the form below. Username, email, and password are
+          required fields.
+        </p>
+        <br></br>
         <input
-          id="userName"
+          id="username"
           minLength={1}
           maxLength={80}
-          name="userName"
-          value={formField.userName}
+          name="username"
+          value={formField.username}
           placeholder="username"
           onChange={onFieldChange}
-        ></input> <br></br>
+        ></input>{" "}
+        <br></br>
         <input
           id="email"
           minLength={1}
@@ -83,7 +89,8 @@ function SignUpForm(props){
           value={formField.email}
           placeholder="email"
           onChange={onFieldChange}
-        ></input> <br></br>
+        ></input>{" "}
+        <br></br>
         <input
           id="password"
           minLength={1}
@@ -92,7 +99,8 @@ function SignUpForm(props){
           value={formField.password}
           placeholder="password"
           onChange={onFieldChange}
-        ></input> <br></br>
+        ></input>{" "}
+        <br></br>
         <input
           id="pronouns"
           maxLength={50}
@@ -100,7 +108,8 @@ function SignUpForm(props){
           value={formField.pronouns}
           placeholder="pronouns"
           onChange={onFieldChange}
-        ></input>  <br></br>
+        ></input>{" "}
+        <br></br>
         <input
           id="location"
           maxLength={50}
@@ -108,7 +117,8 @@ function SignUpForm(props){
           value={formField.location}
           placeholder="city and state"
           onChange={onFieldChange}
-        ></input>  <br></br>
+        ></input>{" "}
+        <br></br>
         <input
           id="className"
           maxLength={50}
@@ -116,7 +126,8 @@ function SignUpForm(props){
           value={formField.className}
           placeholder="class name"
           onChange={onFieldChange}
-        ></input><br></br>
+        ></input>
+        <br></br>
         <input
           id="campus"
           maxLength={50}
@@ -124,7 +135,8 @@ function SignUpForm(props){
           value={formField.campus}
           placeholder="campus"
           onChange={onFieldChange}
-        ></input>  <br></br>
+        ></input>{" "}
+        <br></br>
         <input
           id="bio"
           maxLength={50}
@@ -132,13 +144,12 @@ function SignUpForm(props){
           value={formField.bio}
           placeholder="bio"
           onChange={onFieldChange}
-        ></input><br></br>
+        ></input>
+        <br></br>
         <input type="submit" value="Submit" />
-        </div>
-        <div className="right-half"></div>
-      </form>
-
-      
+      </div>
+      <div className="right-half"></div>
+    </form>
   );
-};
+}
 export default SignUpForm;
