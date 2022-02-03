9879d132b12d2703a4ca087d1819be361f1dd673
Andrea Yuson <andreasyuson@gmail.com>
2022-02-02 21:25:31 -0800

    Added NewPostForm

--- src/components/NewPostForm.js
+++ src/components/NewPostForm.js
@@ -1,97 +1,85 @@
-import './NewPostForm.css'
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import axios from 'axios';
-import AppContext from "../AppContext";
+import React, { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import "./NewPostForm.css";
+import AppContext from "../AppContext"
 
-const NewPostForm = (props) => {
-  const [formField, setFormField] = useState({ title: "", text: "" })
+function NewPostForm(props) {
+  const [formField, setFormField] = useState({ title: "", text: "",user:"" });
   let navigate = useNavigate();
-  
-  const onTitleChange = (e) => {
-    setFormField({
-      ...formField,
-      title: e.target.value
-    });
+  const myContext = useContext(AppContext);
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      accept: "application/json",
+    },
   };
 
-  const onTextChange = (e) => {
+  const onFieldChange = (e) => {
     setFormField({
       ...formField,
-      text: e.target.value
+      [e.target.name]: e.target.value,
     });
   };
 
   const onNewPostFormSubmit = (e) => {
     e.preventDefault();
-    
-    /*We are going into the DOM taking specific element which is the GET element by ID we wew taking that element and bringing it into our function as a local varible so we can do things with it.*/
-    const title = document.getElementById('posTitle');
-    const text = document.getElementById('text');
+    const user = myContext.userVariable
+    const userId = user["user_id"]
+    const title = document.getElementById("title");
+    const text = document.getElementById("text");
 
     let validTitle = true;
     let validText = true;
-    if (formField.title.length ===0 || formField.title.length > 50) {
-      /*ADD CSS FOR THE BORDER TO TURN IT RED MAKE A CSS ID TAG FOR THE BOX*/
-      // potentially add char limit into jsx element so we dont have to have condition logic for too large of an input
-      title.style.borderColor= 'red'; 
+    if (formField.title.length === 0 || formField.title.length > 50) {
+    title.style.borderColor = "red";
       validTitle = false;
-    }; 
-    if (formField.text.length ===0 || formField.text.length > 50) {
-      /*ADD CSS FOR THE BORDER TO TURN IT RED MAKE A CSS ID TAG FOR THE BOX*/
-      text.style.borderColor= 'red';
-      validText = false; 
-    };
+    }
+    if (formField.text.length === 0 || formField.text.length > 50) {
+      text.style.borderColor = "red";
+      validText = false;
+    }
 
-    if (validTitle === true && validText === true) {axios
-      // how do we make sure to send user data with new post so API call makes a new post with user, send props?
-    .post(`${process.env.REACT_APP_BACKEND_URL}/newpost`, formField, props)
-    .then((response) => {
-      if (response.status !== 200) {
-        // give error messgae, stay on post page
-      } else {
-        // props.updateCurrUser(response.data)
-        // give success message
-        
-      }
-      setFormField({
-        userInfo: "",
-        password: "",
-      });
-      // how to we ensure new API call every time user navigates to feed?
-      navigate(`/feed`);
-    })  
-    /*possibly adding logic*/
-    .catch((err) => {
-      console.log(err);
-    });  
-  }};
+    if (validTitle === true && validText=== true) {
+      setFormField({user:userId})
+      console.log(formField);
+      axios
+        .post(`${process.env.REACT_APP_BACKEND_URL}/newpost`, [formField])
+        .then((response) => {
+          console.log(response);
+          
+          navigate(`/feed`);
 
+        })
+        /*possibly adding logic*/
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
 
   return (
-    <div className='new-post-container'>
-      <form className="new-post-form" onSubmit={onNewPostFormSubmit}>
-        <input 
-        id="postTitle"
+    <form className="new-post-form" onSubmit={onNewPostFormSubmit}>
+      <input
+        id="title"
         minLength={1}
         maxLength={80}
-        name="postTitle"
-        value= {formField.title}
-        placeholder="post title" 
-        onChange={onTitleChange}
-        ></input>
-        <input 
+        name="title"
+        value={formField.title}
+        placeholder="title"
+        onChange={onFieldChange}
+      ></input>
+      <input
         id="text"
         minLength={1}
         maxLength={100}
         name="text"
-        value= {formField.text}
-        placeholder="post text"
-        onChange={onTextChange}
-        ></input>
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
+        value={formField.text}
+        placeholder="text"
+        onChange={onFieldChange}
+      ></input>
+      <input className="button" type="submit" value="Submit New Post" />
+    </form>
   );
-};
+}
 export default NewPostForm;
