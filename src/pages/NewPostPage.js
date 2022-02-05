fe907142e18c7d3e13172cf4603b499b7e27f9aa
Andrea Yuson <andreasyuson@gmail.com>
2022-02-05 10:15:31 -0800

    Added footer to each page exclduing lp

--- src/pages/NewPostPage.js
+++ src/pages/NewPostPage.js
@@ -2,12 +2,14 @@ import "./NewPostPage.css";
 import NewPostForm from "../components/NewPostForm";
 import NavBar from "../components/NavBar";
 import AppContext from "../AppContext";
+import FooterEachPage from "../components/FooterEachPage";
 
 const NewPostPage = (props) => {
   return (
     <div className="new-post-page">
       <NavBar />
       <NewPostForm />
+      <FooterEachPage />
     </div>
   );
 };
--- src/pages/ProfilePage.js
+++ src/pages/ProfilePage.js
@@ -1,11 +1,13 @@
 import './ProfilePage.css';
 import NavBar from '../components/NavBar';
 import AppContext from "../AppContext";
+import FooterEachPage from "../components/FooterEachPage"
 
 const ProfilePage = () => {
   return (
     <div className='profile-page'>
       <NavBar />
+      <FooterEachPage />
     </div>
   );
 };
